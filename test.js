8352d9e327485393d9cfcc8de1089bef95c8f004
TaniaCruz0921 <taniacruz.tc1@gmail.com>
2024-06-26 18:35:24 -0600

    hola

---
+++ test.js
@@ -0,0 +1,27 @@
+let a = 5
+let b = 5
+
+let suma = a + b 
+console.log (suma)
+
+let multiplicacion = a + b
+console.log (multiplicacion)
+
+let resta = a + b
+console.log (resta)
+
+let division = a / b 
+console.log (division)
+
+const resultados =
+{
+    suma: suma, 
+    resta: resta,
+    multiplicacion: multiplicacion,
+    division: division,
+}
+
+console.log (resultados.division)
+
+
+
